fd11503f935912d3a5ba62f662b6183da160c4d1
thienph3 <thienph3@gmail.com>
2026-04-17 21:51:33 +0700

    v0.0.3 fix bug

--- lib/features/schedule/screens/schedule_screen.dart
+++ lib/features/schedule/screens/schedule_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 
 import '../../../core/theme/app_colors.dart';
 import '../../../core/constants/app_spacing.dart';
